e69addd8f6c00bb5120731282940eded7dc8abbe
mskvszkyt <71025319+mskvszkyt@users.noreply.github.com>
2024-09-06 10:49:50 +0200

    Kész feladat

---
+++ LegoElem.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickStore
+{
+//    <Item>
+//                                                              <ItemID>52501</ItemID>
+//   <ItemTypeID>P</ItemTypeID>
+//   <ColorID>85</ColorID>
+//                                                              <ItemName>Slope, Inverted 45 6 x 1 Double with 1 x 4 Cutout</ItemName>
+//   <ItemTypeName>Part</ItemTypeName>
+//                                                              <ColorName>Dark Bluish Gray</ColorName>
+//   <CategoryID>32</CategoryID>
+//                                                              <CategoryName>Slope, Inverted</CategoryName>
+//   <Status>I</Status>
+//                                                              <Qty>2</Qty>
+//   <Price>0.000</Price>
+//   <Condition>N</Condition>
+//   <OrigQty>4</OrigQty>
+//  </Item>
+    internal class LegoElem
+    {
+        string id;
+        string name;
+        string category;
+        string color;
+        int quantity;
+
+        //char itemTypeID;
+        //int colorID;
+        //string itemTypeName;
+        //int categoryID;
+        //char status;
+        //double price;
+        //char condition;
+        //int origQty;
+
+        public LegoElem(string id, string name, string categoryName, string colorName, int quantity) {
+            this.id = id;
+            this.name = name;
+            category = categoryName;
+            color = colorName;
+            this.quantity = quantity;
+        }
+
+        public string Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Category { get => category; set => category = value; }
+        public string Color { get => color; set => color = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+
+        //public LegoElem(string id, string name, string category, string color, int quantity, char itemTypeID, int colorID, string itemTypeName, int categoryID, char status, double price, char condition, int origQty)
+        //{
+        //    this.id = id;
+        //    this.name = name;
+        //    this.category = category;
+        //    this.color = color;
+        //    this.quantity = quantity;
+
+
+        //    this.itemTypeID = itemTypeID;
+        //    this.colorID = colorID;
+        //    this.itemTypeName = itemTypeName;
+        //    this.categoryID = categoryID;
+        //    this.status = status;
+        //    this.price = price;
+        //    this.condition = condition;
+        //    this.origQty = origQty;
+        //}
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Xml.Linq;
+using Microsoft.Win32;
+
+namespace BrickStore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<LegoElem> LegoElements = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dgLegoElements.ItemsSource = LegoElements;
+        }
+
+        public void Beolvasas(string fajl)
+        {
+            if (fajl != null)
+            {
+                XDocument xaml = XDocument.Load(fajl);
+                foreach (XElement element in xaml.Descendants("Item"))
+                {
+                    string id = element.Element("ItemID").Value;
+                    string name = element.Element("ItemName").Value;
+                    string colorName = element.Element("ColorName").Value;
+                    string categoryName = element.Element("CategoryName").Value;
+                    int qty = Convert.ToInt32(element.Element("Qty").Value);
+                    LegoElem newLego = new(id, name, colorName, categoryName, qty);
+                    LegoElements.Add(newLego);
+                    lbCount.Content = $"Count: {LegoElements.Count()}";
+                }
+            }
+            else
+            {
+                MessageBox.Show($"Nem áll rendelkezésre \"{fajl}\" nevű fájl!");
+            }
+        }
+
+        private void btnBetoltes_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".bsx";
+            fileDialog.Filter = "BSX Files (.bsx)|*.bsx";
+
+            fileDialog.ShowDialog();
+            Beolvasas(fileDialog.FileName);
+        }
+
+        private void tbSzuro_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var szurt = LegoElements.Where(x => x.Name.ToLower().StartsWith(tbSzuro.Text.ToLower()) || x.Id.ToLower().StartsWith(tbSzuro.Text.ToLower()));
+            dgLegoElements.ItemsSource = szurt;
+            lbCount.Content = $"Count: {szurt.Count()}";
+        }
+    }
+}

445b839e3ebc09a9a8be7715602aa3b5fe3e2801
mskvszkyt <71025319+mskvszkyt@users.noreply.github.com>
2024-09-12 11:06:32 +0200

    szűrő és mappából betöltés

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,15 +20,19 @@ namespace BrickStore
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableCollection<LegoElem> LegoElements = new();
+        ObservableCollection<LegoElem> legoElements = new();
         public MainWindow()
         {
             InitializeComponent();
-            dgLegoElements.ItemsSource = LegoElements;
+            dgLegoElements.ItemsSource = legoElements;
         }
 
         public void Beolvasas(string fajl)
         {
+            if (dgLegoElements.Items.Count != 0)
+            {
+                legoElements.Clear();
+            }
             if (fajl != null)
             {
                 XDocument xaml = XDocument.Load(fajl);
@@ -40,16 +44,16 @@ namespace BrickStore
                     string categoryName = element.Element("CategoryName").Value;
                     int qty = Convert.ToInt32(element.Element("Qty").Value);
                     LegoElem newLego = new(id, name, categoryName, colorName, qty);
-                    LegoElements.Add(newLego);
-                    lbCount.Content = $"Count: {LegoElements.Count()}";
+                    legoElements.Add(newLego);
+                    lbCount.Content = $"Count: {legoElements.Count()}";
                 }
                 cbKategoriaSzuro.Items.Add("All");
-                foreach (var elem in LegoElements.Select(x => x.Category).Distinct())
+                foreach (var elem in legoElements.Select(x => x.Category).Distinct().Order())
                 {
                     cbKategoriaSzuro.Items.Add(elem);
                 }
                 cbSzinSzuro.Items.Add("All");
-                foreach (var elem in LegoElements.Select(x => x.Color).Distinct())
+                foreach (var elem in legoElements.Select(x => x.Color).Distinct().Order())
                 {
                     cbSzinSzuro.Items.Add(elem);
                 }
@@ -64,21 +68,44 @@ namespace BrickStore
 
         private void btnBetoltes_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.DefaultExt = ".bsx";
-            fileDialog.Filter = "BSX Files (.bsx)|*.bsx";
+            OpenFolderDialog folderPicker = new OpenFolderDialog();
+            folderPicker.ShowDialog();
+            //OpenFileDialog fileDialog = new OpenFileDialog();
+            ////fileDialog.DefaultExt = ".bsx";
+            ////fileDialog.Filter = "BSX Files (.bsx)|*.bsx";
+            //fileDialog.ShowDialog();
+            //Beolvasas(fileDialog.FileName);
 
-            fileDialog.ShowDialog();
-            Beolvasas(fileDialog.FileName);
+            foreach (string file in Directory.EnumerateFiles(folderPicker.FolderName, "*.bsx"))
+            {
+
+                lbxFileok.Items.Add(file.Split('\\')[file.Split('\\').Count()-1]);
+            }
         }
 
         public void Szures()
         {
-            string szin = cbSzinSzuro.SelectedItem.ToString() == "All" ? cbSzinSzuro.SelectedItem.ToString() : "";
-            var szurt = LegoElements.Where(x => (x.Name.ToLower().StartsWith(tbSzuro.Text.ToLower()) || x.Id.ToLower().StartsWith(tbSzuro.Text.ToLower()))
-            && x.Category == (cbKategoriaSzuro.SelectedItem.ToString())
-            && x.Color == );
+            string selectedKategoria = cbKategoriaSzuro.SelectedItem.ToString();
+            string selectedSzin = cbSzinSzuro.SelectedItem.ToString();
+            var szurt = legoElements.Where(x => (x.Name.ToLower().StartsWith(tbSzuro.Text.ToLower()) || x.Id.ToLower().StartsWith(tbSzuro.Text.ToLower()))
+            && x.Category.Contains(selectedKategoria == "All" ? "" : selectedKategoria)
+            && x.Color.Contains(selectedSzin == "All" ? "" : selectedSzin));
             dgLegoElements.ItemsSource = szurt;
+
+            cbKategoriaSzuro.Items.Clear();
+            cbSzinSzuro.Items.Clear();
+            cbKategoriaSzuro.Items.Add("All");
+            foreach (var elem in szurt.Select(x => x.Category).Distinct().Order())
+            {
+                cbKategoriaSzuro.Items.Add(elem);
+            }
+            cbSzinSzuro.Items.Add("All");
+            foreach (var elem in szurt.Select(x => x.Color).Distinct().Order())
+            {
+                cbSzinSzuro.Items.Add(elem);
+            }
+            cbSzinSzuro.SelectedItem = selectedSzin;
+            cbKategoriaSzuro.SelectedItem = selectedKategoria;
             lbCount.Content = $"Count: {szurt.Count()}";
         }
 
@@ -89,7 +116,13 @@ namespace BrickStore
 
         private void btnTorol_Click(object sender, RoutedEventArgs e)
         {
-            dgLegoElements.ItemsSource = LegoElements;
+            dgLegoElements.ItemsSource = legoElements;
+        }
+
+        private void lbxFileok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            dgLegoElements.ItemsSource = legoElements;
+            Beolvasas(lbxFileok.SelectedItem.ToString());
         }
     }
 }
